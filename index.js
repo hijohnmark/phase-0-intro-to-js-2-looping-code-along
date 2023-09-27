f5643e24d7aaab2cf5c3d48e3b17262142424671
John Mark Harrell <hijohnmark@gmail.com>
2023-09-27 13:00:13 +0700

    complete intro to js 2 looping code along

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+
+/*
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+*/
+
+function writeCards(array, event) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    
+}
+return newArray;
+}; 
+
+function countDown() {
+    let number = 10
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
