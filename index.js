dd17b92852695a85046e9a388d33e510d57ea438
John Mark Harrell <hijohnmark@gmail.com>
2023-10-25 20:33:09 +0700

    complete js 2 looping code along v2

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Code your solutions in this file
 
-/*
-
-for (let age = 30; age < 40; age++) {
-    console.log(`I'm ${age} years old. Happy birthday to me!`);
-}
 
 
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`); 
+// }
 
-const gifts = ["teddy bear", "drone", "doll"];
+const gifts = ['teddy bear', 'drone', 'doll'];
 
 function wrapGifts(gifts) {
     for (let i = 0; i < gifts.length; i++) {
@@ -21,20 +19,16 @@ function wrapGifts(gifts) {
 
 wrapGifts(gifts);
 
-*/
-
-function writeCards(array, event) {
-    const newArray = [];
-    for (let i = 0; i < array.length; i++) {
-    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
-    
+function writeCards(names, event) {
+    let tyMessages = [];
+    for (let i=0; i < names.length; i++) {
+        tyMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return tyMessages;
 }
-return newArray;
-}; 
 
-function countDown() {
-    let number = 10
-    while (number >= 0) {
-        console.log(number--);
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int--);
     }
 }
